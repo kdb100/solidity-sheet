515028cbf1674751acf623162891146b8a349f6e
kdb100 <oneblueproject@gmail.com>
2021-06-29 12:22:24 -0400

    Initial commit

---
+++ mappings.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: GPL-3.0
+ 
+pragma solidity >=0.5.0 <0.9.0;
+contract Auction{
+    
+    // declaring a variable of type mapping
+    // keys are of type address and values of type uint
+    mapping(address => uint) public bids;
+    
+    // initializing the mapping variable
+    // the key is the address of the account that calles the function
+    // and the value the value of wei sent when calling the function
+    function bid() payable public{
+        bids[msg.sender] = msg.value;
+    }
